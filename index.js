e9420b2ff18e8aaad615b7582f4b32c3799ee50b
alexshaoo <alexshao90@gmail.com>
2023-12-14 15:28:35 -0800

    combination working

--- index.js
+++ index.js
@@ -18,26 +18,43 @@ const rightWall = Bodies.rectangle(800, 300, 60, 600, { isStatic: true });
 
 World.add(engine.world, [ground, leftWall, rightWall]);
 
-function dropFruit(x) {
-  const radius = 20;
-  const y = -radius * 2;
+function dropFruit(x, y, sizeLevel) {
+  const radius = 20 + sizeLevel * 10;
   const fruit = Bodies.circle(x, y, radius, {
     density: 0.001,
     frictionAir: 0.0,
     restitution: 0.8,
     friction: 0.1,
+    sizeLevel: sizeLevel
   });
   World.add(engine.world, fruit);
 }
 
+Matter.Events.on(engine, 'collisionStart', function(event) {
+  let pairs = event.pairs;
+
+  pairs.forEach(function(pair) {
+    let bodyA = pair.bodyA;
+    let bodyB = pair.bodyB;
+
+    if (bodyA.sizeLevel !== undefined && bodyA.sizeLevel === bodyB.sizeLevel) {
+      const newSizeLevel = bodyA.sizeLevel + 1;
+      const newX = (bodyA.position.x + bodyB.position.x) / 2;
+      const newY = (bodyA.position.y + bodyB.position.y) / 2;
+
+      World.remove(engine.world, [bodyA, bodyB]);
+
+      dropFruit(newX, newY, newSizeLevel);
+    }
+  });
+});
+
 render.canvas.addEventListener('mousedown', function(event) {
   const canvasBounds = render.canvas.getBoundingClientRect();
   const scaleX = render.canvas.width / canvasBounds.width;
-  const scaleY = render.canvas.height / canvasBounds.height;
   const canvasX = event.clientX - canvasBounds.left;
-  const canvasY = event.clientY - canvasBounds.top;
 
-  dropFruit(canvasX * scaleX, canvasY * scaleY);
+  dropFruit(canvasX * scaleX, -40, 0);
 });
 
 Render.run(render);
