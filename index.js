782bc29b7303dc4e0f5c336be8e47caa781926dc
alexshaoo <alexshao90@gmail.com>
2023-12-14 15:22:25 -0800

    drop circles

--- index.js
+++ index.js
@@ -12,8 +12,33 @@ const render = Render.create({
   }
 });
 
-const testBox = Bodies.rectangle(400, 200, 80, 80, { isStatic: true });
-World.add(engine.world, [testBox]);
+const ground = Bodies.rectangle(400, 580, 810, 60, { isStatic: true });
+const leftWall = Bodies.rectangle(0, 300, 60, 600, { isStatic: true });
+const rightWall = Bodies.rectangle(800, 300, 60, 600, { isStatic: true });
+
+World.add(engine.world, [ground, leftWall, rightWall]);
+
+function dropFruit(x) {
+  const radius = 20;
+  const y = -radius * 2;
+  const fruit = Bodies.circle(x, y, radius, {
+    density: 0.001,
+    frictionAir: 0.0,
+    restitution: 0.8,
+    friction: 0.1,
+  });
+  World.add(engine.world, fruit);
+}
+
+render.canvas.addEventListener('mousedown', function(event) {
+  const canvasBounds = render.canvas.getBoundingClientRect();
+  const scaleX = render.canvas.width / canvasBounds.width;
+  const scaleY = render.canvas.height / canvasBounds.height;
+  const canvasX = event.clientX - canvasBounds.left;
+  const canvasY = event.clientY - canvasBounds.top;
+
+  dropFruit(canvasX * scaleX, canvasY * scaleY);
+});
 
 Render.run(render);
 Engine.run(engine);
